fe1c032e1850d34813cbd61ea69933387f99392b
JutilaO <oskari.jutila01@gmail.com>
2024-08-20 14:05:58 +0300

    Moduuli5/Tehtävät

---
+++ Moduuli 5/Tehtävä 4.py
@@ -0,0 +1,9 @@
+kaupungit = []
+
+for i in range(5):
+    kaupunki = input("Syötä kaupunki: ")
+    kaupungit.append(kaupunki)
+
+print("Kaupungit:")
+for kaupunki in kaupungit:
+    print(kaupunki)
